02dcc9e0c0a9f14020283bab5bc7a8160e6d07df
Votre nom <votre-email@example.com>
2024-05-22 20:52:46 +0200

    first commit

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 
-app.use('/api/history', sessionRoutes);
+app.use('/api/latest', sessionRoutes);
 
 const startServer = async () => {
   try {
